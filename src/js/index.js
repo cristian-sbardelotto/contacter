a75d5586218b41d722bbc20f9622bd44ecba68b2
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-06-28 15:11:12 -0300

    feat: create basic methods in Agenda class

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+class Agenda {
+  /**
+   * @param {Array} [initialValue] - Initial value of the agenda.
+   */
+  constructor(initialValue) {
+    this.contacts = initialValue ?? [];
+  }
+
+  addContact({ name, phone }) {
+    // TODO: validation on name and phone
+    const id = self.crypto.randomUUID();
+
+    this.contacts.push({ name, phone, id });
+  }
+
+  removeContact(id) {
+    // TODO: check if ID exists
+    const newList = this.contacts.filter(contact => contact.id !== id);
+
+    this.contacts = newList;
+  }
+}
+
+const agenda = new Agenda();
+
+// TODO: save user data in localStorage
+// TODO: form validation
+// TODO: data validation
+// TODO: generate profile picture (random color)
