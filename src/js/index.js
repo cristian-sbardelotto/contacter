2efae124974d4f5a7ea3f0c8bf923f7a4b40e634
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-06-28 21:12:15 -0300

    feat: implement basic handleSubmit fn

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 import { Agenda } from './agenda.js';
 
 const agenda = new Agenda();
-agenda.addContact({ name: 'Paulo', phone: '494340343' });
-console.log(agenda);
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  agenda.addContact(data);
+  console.log(agenda);
+}
