4ffee6fe75b3e27a38ce0419500031fd30891778
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-07-01 15:18:56 -0300

    feat: implement DOM update after agenda change

--- src/js/agenda.js
+++ src/js/agenda.js
@@ -11,6 +11,8 @@ export class Agenda {
     const id = self.crypto.randomUUID();
 
     this.contacts.push({ name, phone, id });
+
+    this.updateDOM();
   }
 
   removeContact(id) {
@@ -18,5 +20,26 @@ export class Agenda {
     const newList = this.contacts.filter(contact => contact.id !== id);
 
     this.contacts = newList;
+
+    this.updateDOM();
+  }
+
+  updateDOM() {
+    const list = document.querySelector('ul.list');
+    list.innerHTML = '';
+
+    this.contacts.forEach(contact => {
+      list.innerHTML += `
+        <li class="item">
+          <div class="profile-picture">${contact.name[0]}</div>
+
+          <div class="contact-info">
+            <h4>${contact.name}</h4>
+
+            <p>${contact.phone}</p>
+          </div>
+        </li> 
+      `;
+    });
   }
 }
